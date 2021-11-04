4df14e777d2c3428264cba017a21018c1f2ea798
mferraris <mgl.ferraris@gmail.com>
2021-11-04 15:53:11 +0100

    rendered producer info on producer profile page

--- src/pages/ProducerProfile.js
+++ src/pages/ProducerProfile.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../store/user/selectors";
 
 export default function ProducerProfile() {
-  return <div>this is the producer profile page</div>;
+  const user = useSelector(selectUser);
+
+  return (
+    <div className="producerProfileGrid">
+      <h1>{user.name}</h1>
+      <img src={user.profileImg} alt={user.name}></img>
+      <h3>Description</h3>
+      <p>{user.description}</p>
+      <h3>Contact</h3>
+      <p>{user.website}</p>
+      <p>{user.email}</p>
+      <p>{user.phone}</p>
+      <h3>Locations</h3>
+      <p>{user.location}</p>
+      <h3>Produces</h3>
+    </div>
+  );
 }
